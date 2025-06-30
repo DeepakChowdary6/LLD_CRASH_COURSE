91216383da1086f83e7f75dffc78fa9ac079a459
DeepakChowdary6 <411877@student.nitandhra.ac.in>
2025-06-30 12:18:42 +0530

    "proxy design pattern"

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,43 @@
 import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.*;
 
 public class Main
 {
     public static void main(String[] args) {
 
+
+//       ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+//
+//        Runnable task = () -> System.out.println("Executed at " + java.time.LocalTime.now());
+//
+//        // Schedule at fixed rate (initial delay = 0s, repeat every 2s)
+//        scheduler.scheduleAtFixedRate(task, 0, 2, TimeUnit.SECONDS);
+
+new Thread(()->{   while(true){
+    System.out.println(" Thread 1");
+//    try {
+//        Thread.sleep(2000);
+//    }catch (Exception e){
+//        System.out.println(e.getMessage());
+//    }
+}}).start();
+
+new Thread(()->{   while(true){
+//    try {
+//        Thread.sleep(2000);
+//    }catch (Exception e){
+//        System.out.println(e.getMessage());
+//    }
+    System.out.println(" Thread 2");
+}}).start();
+new Thread(()->{   while(true){
+//    try {
+//        Thread.sleep(4000);
+//    }catch (Exception e){
+//        System.out.println(e.getMessage());
+//    }
+    System.out.println(" Thread 3");
+}}).start();
 
         //obj.display(); // "Display"
     }
---
+++ src/design_patterns/structural/proxy/Main.java
@@ -0,0 +1,47 @@
+package design_patterns.structural.proxy;
+/*
+* Proxy design pattern intent according to GoF is: Provide a surrogate or placeholder for another object to control access to it.
+* The definition itself is very clear and proxy design pattern is used when we want to provide controlled access of a functionality.
+*/
+interface image{
+    void display();
+}
+class RealImage implements image{
+    String imageName;
+    public RealImage(String imageName){
+        this.imageName=imageName;
+        loadImageFromDisk();
+    }
+   public void loadImageFromDisk() {
+        System.out.println("Loading image: " + imageName);
+    }
+    public void display(){
+        System.out.println("Displaying image "+imageName);
+    }
+}
+
+class ProxyImage implements image{
+    String imageName;
+    RealImage realImage;
+    public ProxyImage(String imageName){
+        this.imageName=imageName;
+    }
+    public void display(){
+        if(realImage==null){
+            realImage=new RealImage(imageName);
+        }
+        realImage.display();
+    }
+    //This code demonstrates how the Proxy Pattern efficiently manages the loading and displaying of images by introducing a proxy that controls access to the real image object,
+    // providing additional functionality such as lazy loading.
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+        image image=new ProxyImage("image1.jpg");
+        image.display();
+        image.display();
+    }
+
+}
